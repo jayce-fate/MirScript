c61e5016408be066a83ae66190c83b73b0041dad
jayce <jayce.fate@gmail.com>
2022-11-13 09:08:44 +0800

    跑不前进

--- game_controller.py
+++ game_controller.py
@@ -8,67 +8,67 @@ import image_processor
 import settings
 
 def one_step_walk_left():
-	# print("one_step_walk_left....")
+	print("one_step_walk_left....")
 	adb_controller.swipe((300,600),(250,600),1000)
 
 def one_step_walk_right():
-	# print("one_step_walk_right....")
+	print("one_step_walk_right....")
 	adb_controller.swipe((300,600),(350,600),1000)
 
 def one_step_walk_up():
-	# print("one_step_walk_up....")
+	print("one_step_walk_up....")
 	adb_controller.swipe((300,600),(300,550),1000)
 
 def one_step_walk_down():
-	# print("one_step_walk_down....")
+	print("one_step_walk_down....")
 	adb_controller.swipe((300,600),(300,650),1000)
 
 def one_step_walk_left_up():
-	# print("one_step_walk_left_up....")
+	print("one_step_walk_left_up....")
 	adb_controller.swipe((300,600),(250,550),1000)
 
 def one_step_walk_right_up():
-	# print("one_step_walk_right_up....")
+	print("one_step_walk_right_up....")
 	adb_controller.swipe((300,600),(350,550),1000)
 
 def one_step_walk_left_down():
-	# print("one_step_walk_left_down....")
+	print("one_step_walk_left_down....")
 	adb_controller.swipe((300,600),(250,650),1000)
 
 def one_step_walk_right_down():
-	# print("one_step_walk_right_down....")
+	print("one_step_walk_right_down....")
 	adb_controller.swipe((300,600),(350,650),1000)
 
 def one_step_run_left():
-	# print("one_step_walk_left....")
+	print("one_step_run_left....")
 	adb_controller.swipe((400,600),(100,600),200)
 
 def one_step_run_right():
-	# print("one_step_walk_right....")
+	print("one_step_run_right....")
 	adb_controller.swipe((400,600),(700,600),200)
 
 def one_step_run_up():
-	# print("one_step_walk_up....")
+	print("one_step_run_up....")
 	adb_controller.swipe((400,600),(400,300),200)
 
 def one_step_run_down():
-	# print("one_step_walk_down....")
+	print("one_step_run_down....")
 	adb_controller.swipe((400,600),(400,900),200)
 
 def one_step_run_left_up():
-	# print("one_step_walk_left_up....")
+	print("one_step_run_left_up....")
 	adb_controller.swipe((400,600),(100,300),200)
 
 def one_step_run_right_up():
-	# print("one_step_walk_right_up....")
+	print("one_step_run_right_up....")
 	adb_controller.swipe((400,600),(700,300),200)
 
 def one_step_run_left_down():
-	# print("one_step_walk_left_down....")
+	print("one_step_run_left_down....")
 	adb_controller.swipe((400,600),(100,900),200)
 
 def one_step_run_right_down():
-	# print("one_step_walk_right_down....")
+	print("one_step_run_right_down....")
 	adb_controller.swipe((400,600),(700,900),200)
 
 def close_target_panel():
@@ -134,6 +134,8 @@ def walk_from_to(from_pos, to_pos):
 
 	abs_move_x = abs(move_x)
 	abs_move_y = abs(move_y)
+
+	move_count = abs_move_x
 	if abs_move_x != 0 and abs_move_y != 0:
 		move_count = abs_move_x
 		if abs_move_y < abs_move_x:
@@ -144,35 +146,68 @@ def walk_from_to(from_pos, to_pos):
 		move_count = abs_move_x
 
 	updated_from_pos = list(from_pos)
-	for index in range(0,move_count):
-		if move_x > 0 and move_y > 0:
-			updated_from_pos[0] = updated_from_pos[0] + 1
-			updated_from_pos[1] = updated_from_pos[1] + 1
-			one_step_walk_right_down()
-		elif move_x > 0 and move_y < 0:
-			updated_from_pos[0] = updated_from_pos[0] + 1
-			updated_from_pos[1] = updated_from_pos[1] - 1
-			one_step_walk_right_up()
-		elif move_x < 0 and move_y > 0:
-			updated_from_pos[0] = updated_from_pos[0] - 1
-			updated_from_pos[1] = updated_from_pos[1] + 1
-			one_step_walk_left_down()
-		elif move_x < 0 and move_y < 0:
-			updated_from_pos[0] = updated_from_pos[0] - 1
-			updated_from_pos[1] = updated_from_pos[1] - 1
-			one_step_walk_left_up()
-		elif move_x > 0 and move_y == 0:
-			updated_from_pos[0] = updated_from_pos[0] + 1
-			one_step_walk_right()
-		elif move_x < 0 and move_y == 0:
-			updated_from_pos[0] = updated_from_pos[0] - 1
-			one_step_walk_left()
-		elif move_x == 0 and move_y > 0:
-			updated_from_pos[1] = updated_from_pos[1] + 1
-			one_step_walk_down()
-		elif move_x == 0 and move_y < 0:
-			updated_from_pos[1] = updated_from_pos[1] - 1
-			one_step_walk_up()
+	print("begin while move_count: {}".format(str(move_count)))
+	while move_count > 0:
+		print("move_count: {}".format(str(move_count)))
+		step = 1
+		if move_count > 1:
+			step = 2
 
-	if to_pos[0] - from_pos[0] != 0 or to_pos[1] - from_pos[1] != 0:
+		if move_x > 0 and move_y > 0:
+			updated_from_pos[0] = updated_from_pos[0] + step
+			updated_from_pos[1] = updated_from_pos[1] + step
+			if step == 2:
+				one_step_run_right_down()
+			else:
+				one_step_walk_right_down()
+		elif move_x > 0 and move_y < 0:
+			updated_from_pos[0] = updated_from_pos[0] + step
+			updated_from_pos[1] = updated_from_pos[1] - step
+			if step == 2:
+				one_step_run_right_up()
+			else:
+				one_step_walk_right_up()
+		elif move_x < 0 and move_y > 0:
+			updated_from_pos[0] = updated_from_pos[0] - step
+			updated_from_pos[1] = updated_from_pos[1] + step
+			if step == 2:
+				one_step_run_left_down()
+			else:
+				one_step_walk_left_down()
+		elif move_x < 0 and move_y < 0:
+			updated_from_pos[0] = updated_from_pos[0] - step
+			updated_from_pos[1] = updated_from_pos[1] - step
+			if step == 2:
+				one_step_run_left_up()
+			else:
+				one_step_walk_left_up()
+		elif move_x > 0 and move_y == 0:
+			updated_from_pos[0] = updated_from_pos[0] + step
+			if step == 2:
+				one_step_run_right()
+			else:
+				one_step_walk_right()
+		elif move_x < 0 and move_y == 0:
+			updated_from_pos[0] = updated_from_pos[0] - step
+			if step == 2:
+				one_step_run_left()
+			else:
+				one_step_walk_left()
+		elif move_x == 0 and move_y > 0:
+			updated_from_pos[1] = updated_from_pos[1] + step
+			if step == 2:
+				one_step_run_down()
+			else:
+				one_step_walk_down()
+		elif move_x == 0 and move_y < 0:
+			updated_from_pos[1] = updated_from_pos[1] - step
+			if step == 2:
+				one_step_run_up()
+			else:
+				one_step_walk_up()
+		move_count = move_count - step
+
+	if to_pos[0] - updated_from_pos[0] != 0 or to_pos[1] - updated_from_pos[1] != 0:
+		print("rom_pos: {}".format(str(updated_from_pos)))
+		print("to_pos: {}".format(str(to_pos)))
 		walk_from_to(tuple(updated_from_pos), to_pos)
