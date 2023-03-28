69b19f03424ec0f0aa1e34a3bfafe06739923ca8
DESKTOP-MRMAHE3\jay <zjlsdj@gmail.com>
2023-03-28 21:56:37 +0800

    等级判断，带v或l都算成功

--- game_controller.py
+++ game_controller.py
@@ -506,7 +506,7 @@ def wait_till_finish_login(max_time, step_time):
     while(True):
         adb_controller.screenshot(settings.screenshot_path)
         lv_area_text = read_lv_area_text().lower()
-        if "lv" in lv_area_text or "v." in lv_area_text or "ly." in lv_area_text:
+        if "l" in lv_area_text or "v" in lv_area_text:
             finish_login = True
             break
 
